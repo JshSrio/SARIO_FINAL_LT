1aa028928e659afdeb0d3d7265e39337d5b0f952
JshSrio <josario@my.cspc.edu.ph>
2026-05-15 03:20:08 +0800

    Initial Flask App

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+
+def home():
+    return """
+    <html>
+    <head>
+        <title>Final Learning Task</title>
+    </head>
+
+    <body style="font-family: Arial; text-align: center; margin-top: 100px;">
+
+        <div style="border: 2px solid black; padding: 20px; width: 300px; margin: auto; border-radius: 10px;">
+            <h2>FINAL LT IN SE2</h2>
+            <h1 style="color: green;">Hello, World!</h1>
+            <p>Flask • Docker • GitHub Actions</p>
+        </div>
+
+    </body>
+    </html>
+    """
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
